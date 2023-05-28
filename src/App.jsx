7bc4ceaf7d3d72a56222e0f43a547c321d5934f9
shofia-bani <banishofia@gmail.com>
2023-05-28 14:51:26 +0700

    create delete button

--- src/App.jsx
+++ src/App.jsx
@@ -76,7 +76,8 @@ function App() {
               }}
               > 
             <input type='checkbox' onChange={() => completeTodo(todo.id)}/>
-            {todo.title} 
+           <p> {todo.title} </p>
+           <button style={{marginLeft:'16px'}}>x</button> 
           </li>
         ))
         }
