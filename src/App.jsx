64647b3ff14d6a0ca42d7b50d1bb05a112e29ee9
shofia-bani <banishofia@gmail.com>
2023-05-28 13:38:59 +0700

    reset list item style

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,9 @@ function App() {
       <ul>
         {
         todos.map((todo) => (
-          <li key={todo.id}> {todo.title} </li>
+          <li key={todo.id} className='todo-item'> 
+          <input type='checkbox'/>
+          {todo.title} </li>
         ))
         }
       </ul>
